91d7f428622a9d472bdafa35bcd149b7828a1244
regan <reg.bussy.s@gmail.com>
2026-08-31 15:35:35 +0200

    add command fftM for fast Fourrier Transform of a signal time Matrix

--- CalcRegPC.c
+++ CalcRegPC.c
@@ -159,6 +159,8 @@ extern float RMath_ach(float x);
 extern float RMath_abs(float x);
 
 //Matrix
+extern int RMath_FFT(Matrix *MAccu,int NumMfct,int NumMa,int NumMb);
+
 extern int FillSphere(Matrix *MAccu,int NumM,int PtLinkM,float Radius,float period);
 extern int DisplayObjectMatrix(Matrix *MAccu,int NumM,int PtLinkM,int DrawingMode);
 static int FillMatrixTwoVariable(int NumM,floactet *CodeOfOneLine, floactet *CodeList);
@@ -234,8 +236,8 @@ float SoundFrequency,SoundAmplitude,SoundDuration,SamplesPerSecond;
 static unsigned char OperatorList[] = "+-*/()=A,_"; //if add then change OpListSize
 static unsigned char MathFunctions[]= "exp_ln_sqrt_Trf_sin_cos_tan_fact_^_ch_sh_th_Re_Im_Int_OSC_acos_asin_ath_atan_ash_ach_abs_mod_arg_key_trp_mtxn_mtxp_fM_sign_?_";
 //									                        	1	  2     3     4     5      6     7      8    9  10  11_12 13 14   15    16     17      18    19     20   21   22    23   24     25   26    27     28     29     30    31  32
-static unsigned char InstructionList[]= "end_print_goto_<_=>_==_>_line_grid_gfxdim_action_box3d_getserial_wait_bsr_rts_putserial_createbtn_clscmd_defM_playsndM_fillM_recsndM_loadsndM_saveM_loadM_wtext_fillsphM_dispobjM_colorgfx_dataM_fctobjM_getindM_modobjM_killbtn_savesndM_copyM_";
-//																0       1      2       3   4    5    6    7      8       9        10               11         12          13     14  15    16             17            18			19         20          21       22            23            24          25       26      27           28            29           30       31             32         33       34			35           36    
+static unsigned char InstructionList[]= "end_print_goto_<_=>_==_>_line_grid_gfxdim_action_box3d_getserial_wait_bsr_rts_putserial_createbtn_clscmd_defM_playsndM_fillM_recsndM_loadsndM_saveM_loadM_wtext_fillsphM_dispobjM_colorgfx_dataM_fctobjM_getindM_modobjM_killbtn_savesndM_copyM_fftM_";
+//																0       1      2       3   4    5    6    7      8       9        10               11         12          13     14  15    16             17            18			19         20          21       22            23            24          25       26      27           28            29           30       31             32         33       34			35           36         37
 //char blabli[]= { {'hello'},0x1,{'hi'},0x0};
 
 //--------------labels
@@ -402,6 +404,8 @@ Code 15 = MAccu [15][N°MAccu] [i 1][j 1]
 //						mod<0 insert M2 in M1 at (-mod) position p
 //						mod=0 : merge matrix M2 at the end of M1 resulting size is M1p+M2p
 //						mod>0 : copy (replace data) at position p=mod
+//fftM0,1,2  Perform the fft of signal fct(t) in M0, the coeff ak and bk are output in M1 and M2. M0,M1 and M2 should have Mn=1. N=M0.p and Kmax=M1.p=M2.p
+
 
 
 //MathFunctions
@@ -1784,6 +1788,52 @@ LoopCodeProgram: //-----------------------------------Loop----------------------
 				}
 			OffsetLine=0;
 			}else{PrintCmd("copyM Matrix!\n");goto EndMain;}
+		}	
+		
+//------------
+		if (CodeOfOneLine[OffsetLine].code==11&&CodeOfOneLine[OffsetLine].value==37) {//fftM
+			if (CodeOfOneLine[OffsetLine+1].code==1&& CodeOfOneLine[OffsetLine+2].code==10 &&
+			CodeOfOneLine[OffsetLine+3].code==1&& CodeOfOneLine[OffsetLine+4].code==10 &&
+			CodeOfOneLine[OffsetLine+5].code==1) {
+			int Mfct=(int)CodeOfOneLine[OffsetLine+1].value;
+			int Ma=(int)CodeOfOneLine[OffsetLine+3].value;
+			int Mb=(int)CodeOfOneLine[OffsetLine+5].value;
+			switch(RMath_FFT(MAccu,Mfct,Ma,Mb)){
+			case 2:
+				PrintCmd("Matrix Mfct not defined\n");
+				goto EndMain;
+				break;
+			case 3:
+				PrintCmd("Matrix Mfct bad size\n");
+				goto EndMain;
+				break;
+			case 4:
+				PrintCmd("Matrix Mfct mtx.n should =1\n");
+				goto EndMain;
+				break;
+			case 5:
+				PrintCmd("Matrix Ma not defined\n");
+				goto EndMain;
+				break;		
+			case 6:
+				PrintCmd("Matrix Ma mtx.n should =1\n");
+				goto EndMain;
+				break;
+			case 7:
+				PrintCmd("Matrix Mb not defined\n");
+				goto EndMain;
+				break;
+			case 8:
+				PrintCmd("Matrix Mb mtx.n should =1\n");
+				goto EndMain;
+				break;
+			case 9:
+				PrintCmd("Matrix Ma and Mb should have same size\n");
+				goto EndMain;
+				break;
+			}
+			OffsetLine=0;
+			}else{PrintCmd("fftM Matrix!\n");goto EndMain;}
 		}	
 		
 //------------
